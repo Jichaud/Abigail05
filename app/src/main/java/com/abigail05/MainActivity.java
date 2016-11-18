1788db770a54f5d69e3cbaffb208580d9b955cd3
root <jich1978@gmail.com>
2016-11-18 10:38:07 -0300

    Initial base

--- app/src/main/java/com/abigail05/MainActivity.java
+++ app/src/main/java/com/abigail05/MainActivity.java
@@ -1,13 +1,30 @@
 package com.abigail05;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button vermasAbi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vermasAbi = (Button)findViewById(R.id.abibtn);
+
+        vermasAbi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent vermasAbi = new Intent(MainActivity.this, abriprodlist.class);
+                startActivity(vermasAbi);
+            }
+        });
     }
+
+
 }
