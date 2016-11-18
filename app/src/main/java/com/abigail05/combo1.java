07d862c8eafef259cc0a8245b4c8b09f7d1a9ec7
root <jich1978@gmail.com>
2016-11-18 15:04:21 -0300

    Initial base

--- app/src/main/java/com/abigail05/combo1.java
+++ app/src/main/java/com/abigail05/combo1.java
@@ -1,13 +1,32 @@
 package com.abigail05;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class combo1 extends AppCompatActivity {
+
+    Button home;
+    Button producto;
+    Button marca;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_combo1);
+
+        home = (Button)findViewById(R.id.btnhome);
+        producto = (Button)findViewById(R.id.btnvprod);
+        marca = (Button)findViewById(R.id.btnvmarca);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent home = new Intent(combo1.this, MainActivity.class);
+                startActivity(home);
+            }
+        });
     }
 }
