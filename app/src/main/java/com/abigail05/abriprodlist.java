f9dbf1f8552ab3cfd42a997c739326703de84f6a
root <jich1978@gmail.com>
2016-11-18 12:09:35 -0300

    Initial base

--- app/src/main/java/com/abigail05/abriprodlist.java
+++ app/src/main/java/com/abigail05/abriprodlist.java
@@ -1,13 +1,27 @@
 package com.abigail05;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class abriprodlist extends AppCompatActivity {
+
+    Button vercombo1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abriprodlist);
+
+        vercombo1 = (Button)findViewById(R.id.btncombo1);
+        vercombo1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent vercombo1 = new Intent(abriprodlist.this,combo1.class);
+                startActivity(vercombo1);
+            }
+        });
     }
 }
