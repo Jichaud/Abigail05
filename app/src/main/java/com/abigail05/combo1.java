51255984fe86c7dd8eda95feec8218e2f797660c
root <jich1978@gmail.com>
2016-11-19 17:16:52 -0300

    Initial base

--- app/src/main/java/com/abigail05/combo1.java
+++ app/src/main/java/com/abigail05/combo1.java
@@ -5,18 +5,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class combo1 extends AppCompatActivity {
 
     Button home;
     Button producto;
     Button marca;
+    Button comprarcombo1;
+    String urlcombo = "https://forms.todopago.com.ar/formulario/commands?command=formulario&m=e68ff9a9f087a16157f80793fc0ca56f";
+    ImageView rojo;
+    ImageView naranja;
+    ImageView lila;
+    ImageButton btnrojo;
+    ImageButton btnnaranja;
+    ImageButton btnlila;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_combo1);
 
+        btnrojo = (ImageButton)findViewById(R.id.colorrojo);
+        btnnaranja = (ImageButton)findViewById(R.id.colornaranja);
+        btnlila = (ImageButton)findViewById(R.id.colorlila);
+        comprarcombo1 = (Button)findViewById(R.id.btncompracombo1);
         home = (Button)findViewById(R.id.btnhome);
         producto = (Button)findViewById(R.id.btnvprod);
         marca = (Button)findViewById(R.id.btnvmarca);
@@ -28,5 +42,49 @@ public class combo1 extends AppCompatActivity {
                 startActivity(home);
             }
         });
+
+        comprarcombo1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent comprarcombo1 = new Intent(combo1.this, webview.class);
+                comprarcombo1.putExtra("string", urlcombo);
+                startActivity(comprarcombo1);
+            }
+        });
+
+
+        rojo = (ImageView)findViewById(R.id.combo1img);
+
+        btnrojo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rojo.setVisibility(View.VISIBLE);
+                naranja.setVisibility(View.GONE);
+                lila.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        naranja = (ImageView)findViewById(R.id.combo1imgnaranja);
+
+        btnnaranja.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                naranja.setVisibility(View.VISIBLE);
+                rojo.setVisibility(View.INVISIBLE);
+                lila.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        lila = (ImageView)findViewById(R.id.combo1imglila);
+
+        btnlila.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lila.setVisibility(View.VISIBLE);
+                naranja.setVisibility(View.INVISIBLE);
+                rojo.setVisibility(View.INVISIBLE);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/abigail05/webview.java
+++ app/src/main/java/com/abigail05/webview.java
@@ -1,13 +1,36 @@
 package com.abigail05;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class webview extends AppCompatActivity {
+
+    private WebView webView;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
+
+        String url = getIntent().getStringExtra("string");
+        webView = (WebView)findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+
     }
+
+
 }
