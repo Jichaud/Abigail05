c8f3cc995ed9f3eb7f48e759a207038347bf5321
root <jich1978@gmail.com>
2016-11-20 17:41:24 -0300

    Initial base

--- app/src/main/java/com/abigail05/webview.java
+++ app/src/main/java/com/abigail05/webview.java
@@ -1,15 +1,21 @@
 package com.abigail05;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class webview extends AppCompatActivity {
 
     private WebView webView;
+    TextView txtcarga;
 
     Button home;
     Button producto;
@@ -24,11 +30,13 @@ public class webview extends AppCompatActivity {
         webView = (WebView)findViewById(R.id.webview);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl(url);
+        webView.setWebViewClient(new MyWebViewClient());
 
-        home = (Button)findViewById(R.id.btnhome);
+        txtcarga = (TextView)findViewById(R.id.txtcargando);
         producto = (Button)findViewById(R.id.btnvprod);
         marca = (Button)findViewById(R.id.btnvmarca);
 
+        home = (Button)findViewById(R.id.btnhome);
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,6 +46,26 @@ public class webview extends AppCompatActivity {
         });
 
 
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            txtcarga.setVisibility(View.GONE);
+            super.onPageFinished(view, url);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            txtcarga.setVisibility(View.VISIBLE);
+            super.onPageStarted(view, url, favicon);
+        }
     }
 
 
