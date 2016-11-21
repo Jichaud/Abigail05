227f7574d3ff83dbec9ad596c3c6d711944515b1
root <jich1978@gmail.com>
2016-11-20 21:51:01 -0300

    Initial base

--- app/src/main/java/com/abigail05/MainActivity.java
+++ app/src/main/java/com/abigail05/MainActivity.java
@@ -5,16 +5,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     Button vermasAbi;
+    TextView txtprueba;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txtprueba = (TextView)findViewById(R.id.pruebatexto);
         vermasAbi = (Button)findViewById(R.id.abibtn);
         vermasAbi.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(vermasAbi);
             }
         });
+
     }
 
 
