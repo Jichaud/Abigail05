d256442d66f2b0fb76351abc1ed6c969e9c443a6
root <jich1978@gmail.com>
2016-11-20 21:59:42 -0300

    Initial base

--- app/src/main/java/com/abigail05/MainActivity.java
+++ app/src/main/java/com/abigail05/MainActivity.java
@@ -7,23 +7,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-
 public class MainActivity extends AppCompatActivity {
 
     Button vermasAbi;
-    TextView txtprueba;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtprueba = (TextView)findViewById(R.id.pruebatexto);
         vermasAbi = (Button)findViewById(R.id.abibtn);
         vermasAbi.setOnClickListener(new View.OnClickListener() {
             @Override
